905479749116a12ceaf5d06d36cdeae08274cbee
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-04-16 15:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue (object, key, value){
   return Object.assign ({}, object, { [key]:value})
 }
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  return [key]='value';
+  
+}
