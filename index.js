d512a1b13186aabfa3650b1a3ab20049751d7703
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-04-16 15:25:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function deleteFromObjectByKey (object, key) {
 delete object.key
   return object 
 }
+function deleteFromObjectByKey (object, key) {
+  Object .assign ( {}, object, delete object.key)
+  return object
+}
